87e3d3062d05f57ed3a27c3dece22461006f5af2
jehyun <jehyun1590@naver.com>
2024-02-21 13:12:41 +0900

    Test::password 업데이트

--- src/test/java/com/sparta/blog/controller/UserControllerTest.java
+++ src/test/java/com/sparta/blog/controller/UserControllerTest.java
@@ -2,7 +2,12 @@ package com.sparta.blog.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sparta.blog.dto.UserRequestDto;
+import com.sparta.blog.dto.UserUpdateRequestDto;
+import com.sparta.blog.entity.User;
 import com.sparta.blog.jwt.JwtUtil;
+import com.sparta.blog.mvc.MockSpringSecurityFilter;
+import com.sparta.blog.repository.UserRepository;
+import com.sparta.blog.security.UserDetailsImpl;
 import com.sparta.blog.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -12,10 +17,19 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.security.Principal;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
@@ -24,6 +38,8 @@ class UserControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
+    private Principal mockPrincipal;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -34,16 +50,37 @@ class UserControllerTest {
     @MockBean
     UserService userService;
 
+    @MockBean
+    UserRepository userRepository;
+
+    @MockBean
+    PasswordEncoder passwordEncoder;
+
+
     @BeforeEach
     public void setup() {
-        mvc = MockMvcBuilders.standaloneSetup(new UserController(userService,jwtUtil))
+        mvc = MockMvcBuilders.standaloneSetup(new UserController(userService, jwtUtil))
                 .apply(springSecurity(new MockSpringSecurityFilter()))
                 .build();
     }
 
+    private User testUser;
+
+    private void mockUserSetup() {
+        // Mock 테스트 유져 생성
+        String username = "user";
+        String password = "password123";
+        String email = "test@naver.com";
+        String info = "info1";
+        testUser = new User(username, password, email, info);
+        UserDetailsImpl testUserDetails = new UserDetailsImpl(testUser);
+        mockPrincipal = new UsernamePasswordAuthenticationToken(testUserDetails, "", testUserDetails.getAuthorities());
+    }
+
+
     @Test
     @DisplayName("회원가입")
-    void test1() throws Exception {
+    void testSignup() throws Exception {
         //given
         UserRequestDto userRequestDto = new UserRequestDto();
         userRequestDto.setEmail("testuser@sparta.com");
@@ -57,4 +94,55 @@ class UserControllerTest {
                         .content(objectMapper.writeValueAsString(userRequestDto)))
                 .andExpect(status().isCreated());
     }
+
+    @Test
+    @DisplayName("사용자 info 업데이트")
+    void testUpdateUserInfo() throws Exception {
+        // Given
+        mockUserSetup();
+
+        Long userId = 1L;
+        String token = "test_token";
+        String updatedInfo = "info 업뎃";
+
+        // When
+        when(jwtUtil.getEmailFromToken(token)).thenReturn("test@naver.com");
+
+        // Then
+        mvc.perform(put("/api/users/{id}/info", userId)
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"info\": \"" + updatedInfo + "\", \"email\": \"test@example.com\", \"password\": \"password123\", \"username\": \"testuser\"}")) // 업데이트할 정보 설정
+                .andExpect(status().isOk());
+    }
+    @Test
+    @DisplayName("사용자 비밀번호 업데이트")
+    void testUpdateUserPassword() throws Exception {
+        // Given
+        mockUserSetup();
+
+        Long userId = 1L;
+        String token = "test_token";
+        String oldPassword = "password123";
+        String newPassword = "newPassword123";
+
+        UserUpdateRequestDto userUpdateRequestDto = new UserUpdateRequestDto();
+        userUpdateRequestDto.setOldPassword(oldPassword);
+        userUpdateRequestDto.setNewPassword(newPassword);
+        userUpdateRequestDto.setNewPasswordCheck(newPassword);
+
+        // When
+        when(jwtUtil.getEmailFromToken(token)).thenReturn("test@naver.com");
+        when(userRepository.findByEmail(anyString())).thenReturn(Optional.of(testUser));
+        when(passwordEncoder.matches(anyString(), anyString())).thenReturn(true);
+
+        // Then
+        mvc.perform(put("/api/users/{id}/password", userId)
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userUpdateRequestDto)))
+                .andExpect(status().isOk());
+    }
+
+
 }
