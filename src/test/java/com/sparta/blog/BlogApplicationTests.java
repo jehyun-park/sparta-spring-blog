a884fdd975062754f840d533304a1566046ab3ac
jehyun <jehyun1590@naver.com>
2024-02-21 13:30:25 +0900

    Test::

--- src/test/java/com/sparta/blog/BlogApplicationTests.java
+++ src/test/java/com/sparta/blog/BlogApplicationTests.java
@@ -2,12 +2,11 @@ package com.sparta.blog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BlogApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
